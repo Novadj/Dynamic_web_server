b9e112bbb0a246b59fa1f85a85847ffab4c77684
Novadj <novadj068@gmail.com>
2024-10-15 23:22:18 +0700

    web_server
    open source

---
+++ Bab10_DynamicWebServer_Python/test_template.py
@@ -0,0 +1,16 @@
+from template_engine import \
+    parse_template, \
+    compile_template
+
+fl = open('dokumen/template.html','r')
+template_text = fl.read()
+
+data_parameter = {
+    'nama': 'Nova Disjayanto',
+    'buah': ['Apel', 'Pisang', 'Nanas']
+}
+
+# Kompilasi template
+tokens = parse_template(template_text)
+output = compile_template(tokens, data_parameter)
+print(output)
